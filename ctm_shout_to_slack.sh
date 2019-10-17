5c6cca511130534e5abe1c81c24a935a14e6b684
Robert Stinnett <robert@robertstinnett.com>
2019-10-17 16:13:58 -0500

    Added support for @here

--- ctm_shout_to_slack.sh
+++ ctm_shout_to_slack.sh
@@ -2,7 +2,6 @@
 #!/bin/bash
 # Integrate Control-M Shouts with Slack
 # Robert Stinnett
-
 # Shout Msg comes through in %2 param
 # Format:  channel,message
 
@@ -10,7 +9,7 @@
 
 
 incoming_msg=$2
-hookurl=<Your Slack Hook URL Goes Here>
+hookurl=<Your Slack URL here>
 
 
 
@@ -19,4 +18,10 @@ message=`echo $incoming_msg | cut -d ',' -f 2`
 
 echo channel=$channel - message=$message >> /var/log/controlm/ctm_shout_slack.log
 
-curl -H "Content-type: application/json" -X POST -d "{\"channel\":\"$channel\",\"text\":\"$message\"}" $hookurl
+if [[ $message = *@here* ]]
+then
+    env -u LD_LIBRARY_PATH curl -H "Content-type: application/json" -X POST -d "{\"channel\":\"$channel\",\"text\":\"<!here> ${message//@here/}\"}" $hookurl
+else
+    env -u LD_LIBRARY_PATH curl -H "Content-type: application/json" -X POST -d "{\"channel\":\"$channel\",\"text\":\"$message\"}" $hookurl
+fi
+
